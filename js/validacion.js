512530d4b2e3eb873b7e28ec1ec996f7d970f99a
andres71195 <asuarez71195@gmail.com>
2022-08-18 11:33:06 -0300

    Cambios 1
    Cambios en validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,45 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const terminos = document.getElementById("terminos");
+
+const registro = document.getElementById("regBtn");
+
+registro.addEventListener('click', (e) => {
+if (nombre.value === '' || apellido.value === '' || email.value === '' || password1.value === ''|| password2.value === '') 
+{ e.preventDefault();
+    showAlertError();
+}
+else {
+    showAlertSuccess();
+}
+
+if (password1.length >= 6) {
+    showAlertSuccess();
+}
+else {
+    e.preventDefault();
+    showAlertError();
+}
+
+if (password1.value !== password2.value) {
+    e.preventDefault();
+    showAlertError(); 
+}
+
+if (terminos.checked) {
+    showAlertSuccess();
+}
+
+else {
+    e.preventDefault();
+    showAlertError();
+}
+
+})
