9c76fc88d419cadf6baf411da8233fdb33e4250d
andres71195 <asuarez71195@gmail.com>
2022-08-21 11:22:30 -0300

    Cambios 2
    Modificaciones en validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -1,10 +1,10 @@
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
-}
+};
 
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
-}
+};
 
 const nombre = document.getElementById("nombre");
 const apellido = document.getElementById("apellido");
@@ -15,35 +15,30 @@ const terminos = document.getElementById("terminos");
 
 const registro = document.getElementById("regBtn");
 
-registro.addEventListener('click', (e) => {
+registro.addEventListener("click", (e) => {
+
 if (nombre.value === '' || apellido.value === '' || email.value === '' || password1.value === ''|| password2.value === '') 
 { e.preventDefault();
-    showAlertError();
-}
-else {
-    showAlertSuccess();
+ showAlertError();
 }
 
-if (password1.length >= 6) {
-    showAlertSuccess();
-}
-else {
+else if (password1.value.length < 6) {
     e.preventDefault();
     showAlertError();
 }
 
-if (password1.value !== password2.value) {
+else if (password1.value !== password2.value) {
     e.preventDefault();
     showAlertError(); 
 }
 
-if (terminos.checked) {
-    showAlertSuccess();
-}
-
-else {
+else if (terminos.checked === false) {
     e.preventDefault();
     showAlertError();
 }
 
-})
+else {
+    showAlertSuccess();
+    }
+
+},);
